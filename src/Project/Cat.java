5bea59010059d38b3a81c82e195b1acd630197e3
Neformaln <vladko-91@inbox.ru>
2016-10-11 19:24:57 +0300

    first commit

---
+++ src/Project/Cat.java
@@ -0,0 +1,14 @@
+package Project;
+
+/**
+ * Created by StudentTC on 06.10.2016.
+ */
+public class Cat {
+    int eyes = 2;
+
+    int run() {
+        return 10;
+
+
+    }
+}
---
+++ src/Project/HelloWorld.java
@@ -0,0 +1,16 @@
+package Project;
+
+/**
+ * Created by StudentTC on 06.10.2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+    }
+}
