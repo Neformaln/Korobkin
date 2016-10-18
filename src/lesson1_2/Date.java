a9a9b4757da2c0793a6c02c1ac158572e3856703
Neformaln <vladko-91@inbox.ru>
2016-10-18 21:19:35 +0300

    Merge remote-tracking branch 'origin/master'

--- src/lesson1_2/Date.java
+++ src/lesson1_2/Date.java
@@ -1,6 +1,8 @@
 package lesson1_2;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by StudentTC on 13.10.2016.
@@ -10,15 +12,29 @@ import java.text.DateFormat;
  а также метод getTime() который вернет текущее время ("07:24")
  */
 
-public class Date {
-   public static void main(String[] args){
-
-       Date date = new Date();
-
-
-
-
-
+public class Date
+{
+    public static void main(String[] args)
+    {
+        String date = getDate();
+        String time = getTime();
+        System.out.println(date);
+        System.out.println(time);
+    }
+    private static String getDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat date = new SimpleDateFormat("dd MMMM yyyy");
+        String t1 = date.format(cal.getTime());
+        return (t1);
 
     }
+    private static String getTime()
+    {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat time = new SimpleDateFormat("HH:mm");
+        String t2 = time.format(cal.getTime());
+        return t2;
+    }
+
 }
